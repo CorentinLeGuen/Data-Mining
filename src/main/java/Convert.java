3d92cf95f55617c8c27576740387da6afd7e61de
CorentinLeGuen <leguen.corentin@mail.com>
2017-11-16 14:24:10 +0100

    Update generation file txt and TAGGER result

--- src/main/java/Convert.java
+++ src/main/java/Convert.java
@@ -3,19 +3,19 @@ package main.java;
 import com.saxonica.xqj.SaxonXQDataSource;
 
 import javax.xml.xquery.*;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
+import java.io.*;
 
 public class Convert {
-    private final static String pathName = "xml/";
-    private final static String fileName = "inflammation";
+    private final static String pathName = "xml";
+    private final static String fileName = "alcoholism";
     private final static String extensionName = "xml";
+
+    private final static String command = "tree-tagger-linux-3.2.1/bin/tree-tagger";
 
     public static void main(String[] args) throws Exception {
         SaxonXQDataSource ds = new SaxonXQDataSource();
         XQConnection con = ds.getConnection();
-        String query = "let $fdd := doc(\"" + pathName + fileName + "." + extensionName + "\")/PubmedArticles/PubmedArticle\n" +
+        String query = "let $fdd := doc(\"" + pathName + "/" + fileName + "." + extensionName + "\")/PubmedArticles/PubmedArticle\n" +
                 "for $article in $fdd\n" +
                 "return ('\nT.',$article//ArticleTitle/text(), '\nA.',$article//Abstract/AbstractText/text())";
         XQPreparedExpression expr = con.prepareExpression(query);
@@ -26,7 +26,7 @@ public class Convert {
         if (f.createNewFile()){
             System.out.println("File " + f.getName() + " is created!");
         } else {
-            System.out.println("File " + f.getName() + "already exists.");
+            System.out.println("File " + f.getName() + " already exists.");
         }
 
         try {
@@ -40,7 +40,33 @@ public class Convert {
         }
 
         result.close();
+
         expr.close();
         con.close();
+
+        InputStream stream = null;
+
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder();
+            processBuilder.redirectErrorStream(true);
+            processBuilder.command(command, "files/english-utf8.par", "files/" + fileName + ".txt", "files/" + fileName + "Result.txt");
+
+            Process p = processBuilder.start();
+
+            stream = p.getInputStream();
+
+            int i;
+            char c;
+            while((i = stream.read())!=-1) {
+                c = (char)i;
+                System.out.print(c);
+            }
+
+        } catch (Exception e) {
+            System.err.println("Error in the execution of " + command + " : " + e.getMessage());
+        } finally {
+            if (stream != null)
+                stream.close();
+        }
     }
 }
