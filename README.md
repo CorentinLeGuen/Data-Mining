e45242b6bed16b867ff28bd5decdd9f86c0c6a53
CorentinLeGuen <leguen.corentin@mail.com>
2017-11-16 13:55:45 +0100

    Updating file txt creation

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ Configuration des jar *saxon* et *xqj*.
 
 ![Configuration](img/config.png)
 
+# Tree TAGGER
+
+Télécharger Tree TAGGER à l'adresse [suivante](http://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/).
+
--- src/main/java/Convert.java
+++ src/main/java/Convert.java
@@ -3,20 +3,41 @@ package main.java;
 import com.saxonica.xqj.SaxonXQDataSource;
 
 import javax.xml.xquery.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 
 public class Convert {
-    private final static String pathName = "xml/inflammation.xml";
+    private final static String pathName = "xml/";
+    private final static String fileName = "inflammation";
+    private final static String extensionName = "xml";
 
     public static void main(String[] args) throws Exception {
         SaxonXQDataSource ds = new SaxonXQDataSource();
         XQConnection con = ds.getConnection();
-        String query = "let $fdd := doc(\"" + pathName + "\")/PubmedArticles/PubmedArticle\n" +
+        String query = "let $fdd := doc(\"" + pathName + fileName + "." + extensionName + "\")/PubmedArticles/PubmedArticle\n" +
                 "for $article in $fdd\n" +
                 "return ($article//ArticleTitle/text(), $article//Abstract/AbstractText/text())";
         XQPreparedExpression expr = con.prepareExpression(query);
         XQSequence result = expr.executeQuery();
 
-        System.out.println(result.getSequenceAsString(null));
+        File f = new File("files/" + fileName + ".txt");
+
+        if (f.createNewFile()){
+            System.out.println("File " + f.getName() + " is created!");
+        } else {
+            System.out.println("File " + f.getName() + "already exists.");
+        }
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(f));
+
+            writer.write(result.getSequenceAsString(null));
+
+            writer.close();
+        } catch (Exception e) {
+            System.err.println("Error while writing in the file " + fileName + " : " + e.getMessage());
+        }
 
         result.close();
         expr.close();
